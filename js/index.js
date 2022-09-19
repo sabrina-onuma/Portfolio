43935bd100e429d70c8402f39d493675ce7486d7
sabrina-onuma <Sabrina.onuma@gmail.com>
2022-09-19 15:08:07 -0500

    downloading project slide decks

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-$("a").hover(function(){
+$(".nav-link").hover(function(){
     $(this).css("color","#B15318");},
     function(){
     $(this).css("color","#1c1c1c");
 
 })  
+$(".projB").hover(function(){
+    $(this).css("color","#B15318");},
+    function(){
+    $(this).css("color","#FFFFFF");
+
+})  
+$(document).ready(function () {
+    $("#chumB").click(function () {
+        window.open(location.href = "/images/CHUM_SlideDeck.pdf");
+        return false;
+    });
+});
+$(document).ready(function () {
+    $("#mwB").click(function () {
+        window.open(location.href = "/images/MW_SlideDeck.pdf");
+        return false;
+    });
+});
+$(document).ready(function () {
+    $("#mentorB").click(function () {
+        window.open(location.href = "/images/Mentor_SlideDeck.pdf");
+        return false;
+    });
+});
+
 $("#pSection").click(function(event) {
         
     if (this.hash !== "") {
